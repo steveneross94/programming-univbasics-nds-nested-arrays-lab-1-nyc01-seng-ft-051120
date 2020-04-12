c2900c8a5b42de0a43505b90e77d2624726a7618
Steven Ross <ross.steven@me.com>
2020-04-12 14:02:04 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -1,5 +1,4 @@
 # Examples inspired by U. S. National Organic Standards
-require 'pry'
 
 ORGANIC_PRODUCE = [
   "Strawberries",
@@ -47,7 +46,6 @@ def matrix_lookup(matrix, row, column)
     ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
-  binding.pry
 end
 
 def matrix_update(matrix, row, column, new_value)
