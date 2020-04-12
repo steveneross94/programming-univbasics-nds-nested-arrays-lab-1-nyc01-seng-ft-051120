41cdb3089acc13aa5d08c6b888bbd9bd74866f28
Steven Ross <ross.steven@me.com>
2020-04-12 13:54:05 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,7 +36,7 @@ def sorted_matrix
     ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"],
     ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
   ]
-  produce.sort
+  produce.sort!
 end
 
 def matrix_lookup(matrix, row, column)
